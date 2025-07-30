106ddfce84c7521de05bf63d6f6e0b99838c7f71
thuytrang2201 <thuytrang064p@gmail.com>
2025-07-30 19:22:21 +0700

    update code BTVN buổi 8

--- Automationtest_Selenium/src/main/java/automation/testsuite/TS_PracticeLocateElement_Bai2.java
+++ Automationtest_Selenium/src/main/java/automation/testsuite/TS_PracticeLocateElement_Bai2.java
@@ -7,27 +7,37 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
 
 public class TS_PracticeLocateElement_Bai2 extends CommonBase {
+    // locator trường name
     @Test
     public void getElementById1()
     {
         driver = initChromeDriver(CT_PageURL.AUTOMATIONFC_URL);
-        WebElement emailElement = driver.findElement(By.id("mail"));
-        System.out.println("emailElement:" + emailElement);
+        WebElement namelElement = driver.findElement(By.id("name"));
+        System.out.println("namelElement:" + namelElement);
     }
-
+    // locator trường address
     @Test
     public void getElementById2()
     {
         driver = initChromeDriver(CT_PageURL.AUTOMATIONFC_URL);
-        WebElement disable_passwordElement = driver.findElement(By.id("disable_password"));
-        System.out.println("disable_passwordElement:" + disable_passwordElement);
+        WebElement addressElement = driver.findElement(By.id("address"));
+        System.out.println("addressElement:" + addressElement);
     }
-
+    // locator trường email
     @Test
     public void getElementById3()
     {
         driver = initChromeDriver(CT_PageURL.AUTOMATIONFC_URL);
-        WebElement numberElement = driver.findElement(By.id("number"));
-        System.out.println("numberElement:" + numberElement);
+        WebElement emailElement = driver.findElement(By.id("email"));
+        System.out.println("emailElement:" + emailElement);
+    }
+    // locator trường password
+
+    @Test
+    public void getElementById4()
+    {
+        driver = initChromeDriver(CT_PageURL.AUTOMATIONFC_URL);
+        WebElement passwordElement = driver.findElement(By.id("password"));
+        System.out.println("passwordElement:" + passwordElement);
     }
 }
